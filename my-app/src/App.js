0ef1bf1114ff2479899d870ffb92cb908d4d33bb
amichalo421 <andre.michalowski@gmail.com>
2022-08-17 16:49:34 -0400

    Header and Footer Base

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import SpacesList from "./features/spaces/SpacesList";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <SpacesList />
       <h1> Test </h1>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,14 +1,14 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
-// import NucampLogo from '../app/assets/img/logo.png';
+import { Navbar, NavbarBrand } from "reactstrap";
+import cblogo from '../app/assets/img/cblogo.png';
 
 const Header = () => {
-    return (
-        <Navbar dark color='primary' sticky='top' expand='md'>
-            <NavbarBrand href='/'>
-                <img src={NucampLogo} alt='nucamp logo' />
-            </NavbarBrand>
-        </Navbar>
-    );
+  return (
+    <Navbar dark color="primary" sticky="top" expand="md">
+      <NavbarBrand href="/">
+        <img src={cblogo} alt="nucamp logo" />
+      </NavbarBrand>
+    </Navbar>
+  );
 };
 
 export default Header;
